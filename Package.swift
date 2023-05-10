8b24ccdffb3adf929a98ba09149bc77076544b20
nhatpv <nhatpv@vietmap.vn>
2023-05-10 17:25:20 +0700

    Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -6,21 +6,14 @@ let package = Package(
     products: [
         .library(
             name: "Mapbox",
-            targets: ["Mapbox"]),
-        .library(
-            name: "MapboxMobileEvents",
-            targets: ["MapboxMobileEvents"])  
+            targets: ["Mapbox"])
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/nhatpham0301/map-ios-sdk/raw/main/Mapbox.xcframework.zip",
-            checksum: "e87e06d11c4b5a73643061538c939c6c6190c7a3a818598135b34c19caa507c9"),
-        .binaryTarget(
-            name: "MapboxMobileEvents",
-            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.9.0/MapboxMobileEvents-5.9.0.zip",
-            checksum: "a96842857465adc2f2f3eeaa94dff25e58875bddd871e1a9edeef325591d111a")   
+            url: "https://github.com/nhatpham0301/map-ios-sdk/raw/main/Mapbox_1.0.1.xcframework.zip",
+            checksum: "3bbac3f0358030eabe8fa3a4ec1593f0a8222f93d6d20faeb8c87aa48784e267") 
             ]
 )
